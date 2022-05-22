cb8891c84cb471a7f6367294fc23ca0afc47de14
Liwei <liliwei25@hotmail.com>
2022-05-22 17:50:30 +0800

    Update user service and controller for upload endpoint

--- src/main/java/com/govtech/assignment/controllers/UserController.java
+++ src/main/java/com/govtech/assignment/controllers/UserController.java
@@ -2,8 +2,13 @@ package com.govtech.assignment.controllers;
 
 import com.govtech.assignment.dtos.UserGetRequest;
 import com.govtech.assignment.dtos.UserGetResponse;
+import com.govtech.assignment.dtos.UserUploadRequest;
+import com.govtech.assignment.dtos.UserUploadResponse;
 import com.govtech.assignment.entities.User;
+import com.govtech.assignment.enums.RequestSuccess;
 import com.govtech.assignment.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -11,14 +16,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.async.DeferredResult;
 
 import javax.validation.Valid;
+import java.io.IOException;
 import java.util.List;
-
+import java.util.concurrent.ForkJoinPool;
 
 @Controller
 @Validated
 public class UserController {
+    private final Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     private UserService userService;
 
@@ -26,5 +35,29 @@ public class UserController {
     public @ResponseBody ResponseEntity<UserGetResponse> getAllUsers(@Valid UserGetRequest request) {
         List<User> users = userService.findUsers(request);
         return ResponseEntity.status(HttpStatus.OK).body(new UserGetResponse(users));
+    }
+
+    @PostMapping(
+            path="/upload",
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public @ResponseBody
+    DeferredResult<ResponseEntity<UserUploadResponse>> uploadUsers(UserUploadRequest request) {
+        DeferredResult<ResponseEntity<UserUploadResponse>> output = new DeferredResult<>();
+        logger.info("Processing upload request...");
+        ForkJoinPool.commonPool().submit(() -> {
+            UserUploadResponse response = new UserUploadResponse();
+            try {
+                this.userService.saveAllUsersFromRequest(request);
+                response.setSuccess(RequestSuccess.SUCCESS);
+                output.setResult(ResponseEntity.status(HttpStatus.OK).body(response));
+                logger.info("Upload request processed successfully");
+            } catch (IOException e) {
+                response.setError(e.getMessage());
+                output.setResult(ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response));
+                logger.error("Failed to process upload request: " + e.getMessage());
+            }
+        });
+        return output;
     }
 }
--- src/main/java/com/govtech/assignment/services/UserService.java
+++ src/main/java/com/govtech/assignment/services/UserService.java
@@ -1,19 +1,83 @@
 package com.govtech.assignment.services;
 
 import com.govtech.assignment.dtos.UserGetRequest;
+import com.govtech.assignment.dtos.UserUploadRequest;
 import com.govtech.assignment.entities.User;
 import com.govtech.assignment.repositories.UserRepository;
+import com.govtech.assignment.utilities.CsvHelper;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.apache.commons.lang.math.NumberUtils;
+import org.apache.logging.log4j.util.Strings;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor(onConstructor = @__(@Lazy))
 public class UserService {
+    private static final String[] VALID_CSV_HEADERS = { "NAME", "SALARY" };
+    private static final String ERROR_FILE_DATA_IS_CORRUPTED = "File data is corrupted";
+    private static final String ERROR_INVALID_FILE_TYPE = "Invalid File Type";
+
+    private final Logger logger = LoggerFactory.getLogger(UserService.class);
+
     @Autowired
     private UserRepository userRepository;
 
+    @Lazy
+    private final UserService self;
+
     public List<User> findUsers(UserGetRequest request) {
         return this.userRepository.findUsersFromRequest(request);
+    }
+
+    public void saveAllUsersFromRequest(UserUploadRequest request) throws IOException {
+        validateRequest(request);
+        CSVParser parser = CsvHelper.getParserFromFile(request.getFile());
+        CsvHelper.validateCsvHeaders(parser, Arrays.asList(VALID_CSV_HEADERS));
+        saveUsersFromCsvParser(parser);
+    }
+
+    private void saveUsersFromCsvParser(CSVParser parser) throws IOException {
+        List<User> users = new ArrayList<>();
+        for (CSVRecord record : parser) {
+            User user = convertCsvRecordToUser(record);
+            if (user != null) {
+                users.add(user);
+            }
+        }
+        self.saveUsers(users);
+    }
+
+    private User convertCsvRecordToUser(CSVRecord record) throws IOException {
+        String name = record.get(VALID_CSV_HEADERS[0]);
+        String salaryString = record.get(VALID_CSV_HEADERS[1]);
+        if (Strings.isEmpty(name) || !NumberUtils.isNumber(salaryString)) {
+            throw new IOException(ERROR_FILE_DATA_IS_CORRUPTED);
+        }
+        double salary = Double.parseDouble(salaryString);
+        return salary < 0 ? null : new User(name, salary);
+    }
+
+    private void validateRequest(UserUploadRequest request) throws IOException {
+        if (request == null || request.getFile() == null || !CsvHelper.isCSVFormat(request.getFile())) {
+            logger.error("Invalid User Upload Request");
+            throw new IOException(ERROR_INVALID_FILE_TYPE);
+        }
+    }
+
+    @Transactional
+    public void saveUsers(Iterable<User> users) {
+        this.userRepository.saveAll(users);
     }
 }
